cc3760357883e1a4c51a880b2979a6c8dcb999a8
Codyo <cody@confusionfree.com>
2013-05-20 17:17:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jellypot::Application.config.secret_token = 'cd8b4fd5120901f85d0db58385cc1c542cae66bd1c1fc7d9b7e3f4b3c359bd5f9dc2e808aab306aa817606acb218ec4060e03d41fd6b16d904c7b42b1f0dcd70'
